6cb82583a23bf44d1f8c656ec558ddcc717fd351
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-01-20 21:27:49 +0300

    Add travelTime function with assertion for speed

---
+++ usingAssert.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include <cassert>
+
+// Function to calculate travel time
+float travelTime(float distance, float speed) {
+    assert(speed > 0 && "Speed must be greater than zero.");
+    return distance / speed;
+}
